ece6f875bda8947cb6865b7c9ca3128774a1a3d9
Nasr-eddine Hajji <nhajji@c3r9p3.1337.ma>
2026-09-03 19:54:14 +0100

    by

--- Shell01/ex07/r_dwssap.sh
+++ Shell01/ex07/r_dwssap.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cat /etc/passwd | grep -v '^#' | awk 'NR % 2 == 0' | cut -d: -f1 | rev | sort -r | sed -n "${FT_LINE1},${FT_LINE2}p" | paste -sd, - | sed 's/,/, /g' | sed 's/$/./' | tr -d '\n'
+cat /etc/passwd | grep -v '^#' | awk -F: 'NR % 2 == 0 {print $1}' | rev | sort -r | awk -v a="$FT_LINE1" -v b="$FT_LINE2" 'NR >= a && NR <= b' | tr '\n' ',' | sed 's/,$/./' | sed 's/,/, /g' | tr -d '\n'
